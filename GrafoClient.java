0b1aea49b6bf75222623b19e6bf463374650b5e1
NAULNB <luanbn27@hotmail.com>
2017-05-29 08:18:38 -0300

    Add files via upload
    Parte 1 do trabalho final

--- GrafoClient.java
+++ GrafoClient.java
@@ -22,38 +22,17 @@ public class GrafoClient {
 
   public static void main(String [] args) {
 
-    if (args.length != 1) {
-      System.out.println("Please enter 'simple' or 'secure'");
-      System.exit(0);
-    }
-
     try {
       TTransport transport;
-      if (args[0].contains("simple")) {
-        transport = new TSocket("localhost", 2790);
+     
+        transport = new TSocket("localhost", 2733);
         transport.open();
-      }
-      else {
-        /*
-         * Similar to the server, you can use the parameters to setup client parameters or
-         * use the default settings. On the client side, you will need a TrustStore which
-         * contains the trusted certificate along with the public key. 
-         * For this example it's a self-signed cert. 
-         */
-        TSSLTransportParameters params = new TSSLTransportParameters();
-        params.setTrustStore("../../lib/java/test/.truststore", "thrift", "SunX509", "JKS");
-        /*
-         * Get a client transport instead of a server transport. The connection is opened on
-         * invocation of the factory method, no need to specifically call open()
-         */
-        transport = TSSLTransportFactory.getClientSocket("localhost", 9091, 0, params);
-      }
 
       TProtocol protocol = new  TBinaryProtocol(transport);
       Grafo.Client client = new Grafo.Client(protocol);
 
       perform(client);
-
+      
       transport.close();
     } catch (TException x) {
       x.printStackTrace();
@@ -62,20 +41,17 @@ public class GrafoClient {
 
   private static void perform(Grafo.Client client) throws TException
   {
-   
-    
+       
       int i;
       i= client.GerVertice(1, 27, "Primeiro Vertice", 2.25, 1);
       client.GerVertice(1,0,"",0,2);
       i= client.GerVertice(2, 27, "Segundo Vertice", 7.25, 1);
       client.GerVertice(2,0,"",0,2);
       
-      i=client.GerAresta(1, 2, 13.3, "1", "primeira aresta", 1);
+
+      i=client.GerAresta(1, 2, 13.3, "P", "primeira aresta", 1);
       client.GerAresta(0,0,0,"", "primeira aresta", 2);
-      
-      
-      
-      
+
       
   }
 }
--- GrafoServer.java
+++ GrafoServer.java
@@ -38,15 +38,8 @@ public class GrafoServer {
           simple(processor);
         }
       };  
-      
-      Runnable secure = new Runnable() {
-        public void run() {
-          secure(processor);
-        }
-      };
-     
+
       new Thread(simple).start();
-      new Thread(secure).start();
     } catch (Exception x) {
       x.printStackTrace();
     }
@@ -54,7 +47,7 @@ public class GrafoServer {
 
     public static void simple(Grafo.Processor processor) {
     try {
-      TServerTransport serverTransport = new TServerSocket(2790);
+      TServerTransport serverTransport = new TServerSocket(2733);
       TServer server = new TThreadPoolServer(new TThreadPoolServer.Args(serverTransport).processor(processor));
 
       // Use this for a multithreaded server
@@ -67,35 +60,6 @@ public class GrafoServer {
     }
   }
 
-  public static void secure(Grafo.Processor processor) {
-    try {
-      /*
-       * Use TSSLTransportParameters to setup the required SSL parameters. In this example
-       * we are setting the keystore and the keystore password. Other things like algorithms,
-       * cipher suites, client auth etc can be set. 
-       */
-      TSSLTransportParameters params = new TSSLTransportParameters();
-      // The Keystore contains the private key
-      params.setKeyStore("../../lib/java/test/.keystore", "thrift", null, null);
-
-      /*
-       * Use any of the TSSLTransportFactory to get a server transport with the appropriate
-       * SSL configuration. You can use the default settings if properties are set in the command line.
-       * Ex: -Djavax.net.ssl.keyStore=.keystore and -Djavax.net.ssl.keyStorePassword=thrift
-       * 
-       * Note: You need not explicitly call open(). The underlying server socket is bound on return
-       * from the factory class. 
-       */
-      TServerTransport serverTransport = TSSLTransportFactory.getServerSocket(9091, 0, null, params);
-      TServer server = new TThreadPoolServer(new TThreadPoolServer.Args(serverTransport).processor(processor));
-
-      // Use this for a multi threaded server
-      // TServer server = new TThreadPoolServer(new TThreadPoolServer.Args(serverTransport).processor(processor));
-
-      System.out.println("Starting the secure server...");
-      server.serve();
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-  }
+ 
+  
 }
